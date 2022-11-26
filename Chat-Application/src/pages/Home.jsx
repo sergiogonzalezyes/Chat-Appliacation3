74922385293a99b6654351068f7e606843931040
MikeProduce <105679025+MikeProduce@users.noreply.github.com>
2022-11-25 20:57:16 -0700

    more changes

--- Chat-Application/src/pages/Home.jsx
+++ Chat-Application/src/pages/Home.jsx
@@ -1,18 +1,11 @@
 
+
+
 export const Home = () => {
     return <section>
             <div class="container">
-    <div class="box">
-        <div class="title">
-            <span class="block"></span>
-            <h1>Hello<span></span></h1>
-        </div>
-        <div class="role">
-            <div class="block"></div>
-            <p>Welcome</p>
-        </div>
-    </div>
-</div>
+                <img src="" alt="" />
+            </div>
         </section>
    
 };
