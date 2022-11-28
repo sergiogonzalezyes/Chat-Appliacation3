56c5f10a4156a837f54aaf6ce89196ae28eb1b17
MikeProduce <105679025+MikeProduce@users.noreply.github.com>
2022-11-28 14:54:39 -0700

    finally made it mobile responsive

--- Chat-Application/src/components/pagelayout.jsx
+++ Chat-Application/src/components/pagelayout.jsx
@@ -7,11 +7,11 @@ import React from "react"
 export const PageLayout = (props) => {
 return (<div class="container_1">
   <section className="First_Section_1">
-  <div className="Describe_Web_Site_1">
+  <div className="Home_Image">
       <p className="Title_1">{props.title}</p>
-      <p>{props.text}</p>
+      <p className="Text_Prop">{props.text}</p>
     </div>
-    <div>
+    <div className="Describe_Web_Site_1">
       <img src={props.img} alt="" className="Layout_Image" />
     </div>
   </section>
