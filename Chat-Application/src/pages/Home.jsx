a44f5983976dd0b9cf84d95b038176c660c728f4
MikeProduce <105679025+MikeProduce@users.noreply.github.com>
2022-11-27 21:09:57 -0700

    first part of the image, have to fix the rest of the body

--- Chat-Application/src/pages/Home.jsx
+++ Chat-Application/src/pages/Home.jsx
@@ -19,12 +19,12 @@ export const Home = () => {
 
     return (<div class="container">
         <section className="First_Section">
-                <div>
+                <div className="Home_Image">
                 <img src={computerGuy} alt=""  className="computerGuy"/>
                </div>
                <div className="Describe_Web_Site">
                 <p className="Title">Aliquet lectus proin nibh nisl condimentum id venenatis a</p>
-                <p className="Bottom_title">Morbi leo urna molestie at elementum eu facilisis. Lacus sed viverra tellus in hac. Velit dignissim sodales ut eu sem integer vitae justo eget.</p>
+                <p className="Title_Description">Morbi leo urna molestie at elementum eu facilisis. Lacus sed viverra tellus in hac. Velit dignissim sodales ut eu sem integer vitae justo eget.</p>
                 <button className="Home_Buttons">Login</button>
                 <button className="Home_Buttons_1">Create Account</button>
                </div>
