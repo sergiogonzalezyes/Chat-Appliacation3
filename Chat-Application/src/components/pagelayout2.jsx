443b1356cd5c3c0c824d37c83e53e4bb29c4c370
MikeProduce <105679025+MikeProduce@users.noreply.github.com>
2022-11-29 09:46:47 -0700

    fixed alot of bugs with responsive design, stilll has one more

--- Chat-Application/src/components/pagelayout2.jsx
+++ Chat-Application/src/components/pagelayout2.jsx
@@ -2,8 +2,8 @@ import React from "react"
 
 export const PageLayout2 = (props) => {
 return (<div class="container_1">
-  <section className="First_Section_1">
-  <div className="Describe_Web_Site_1">
+  <section className="First_Section_2">
+  <div className="Describe_Web_Site_2">
       <img src={props.img} alt="" className="Layout_Image" />
     </div>
   <div className="Home_Image">
--- Chat-Application/src/pages/Home.jsx
+++ Chat-Application/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { PageLayout } from "../components/pagelayout";
 import { PageLayout2 } from "../components/pagelayout2";
 import computerGuy from "../images/section1.png";
 import Travel from "../images/Saly-1.png";
-import World from "../images/Saly-40.png";
+import Girl from "../images/Saly-22.png";
 import SpaceShip from "../images/Saly-43.png";
 import { useNavigate } from "react-router-dom";
 
@@ -25,13 +25,15 @@ export const Home = () => {
                <div className="Describe_Web_Site">
                 <p className="Title">Aliquet lectus proin nibh nisl condimentum id venenatis a</p>
                 <p className="Title_Description">Morbi leo urna molestie at elementum eu facilisis. Lacus sed viverra tellus in hac. Velit dignissim sodales ut eu sem integer vitae justo eget.</p>
+                <div className="Button_Container">
                 <button className="Home_Buttons">Login</button>
                 <button className="Home_Buttons_1">Create Account</button>
+                </div>
                </div>
         </section>
             <PageLayout img={Travel} title="Nunc sed augue lacus viverra." text="Nam aliquam sem et tortor consequat id. Molestie nunc non blandit massa enim nec. Nunc sed augue lacus viverra. Ultrices dui sapien eget mi."/>
-            <PageLayout2 img={SpaceShip} title="Donec et odio pellentesque diam." text="  Tortor pretium viverra suspendisse potenti nullam ac tortor vitae purus. Luctus venenatis lectus magna fringilla urna porttitor rhoncus dolor. Sem et tortor consequat id porta nibh venenatis cras."/>  
-            <PageLayout img={World} title="Amet facilisis magna etiam tempor orci." text="Libero volutpat sed cras ornare. Sodales neque sodales ut etiam sit amet nisl purus in. Elementum eu facilisis sed odio morbi quis commodo odio aenean."/>
+            <PageLayout2 img={SpaceShip} title="Donec et odio pellentesque diam." text="Tortor pretium viverra suspendisse potenti nullam ac tortor vitae purus. Luctus venenatis lectus magna fringilla urna porttitor rhoncus dolor. Sem et tortor consequat id porta nibh venenatis cras."/>  
+            <PageLayout img={Girl} title="Amet facilisis magna etiam tempor orci." text="Libero volutpat sed cras ornare. Sodales neque sodales ut etiam sit amet nisl purus in. Elementum eu facilisis sed odio morbi quis commodo odio aenean."/>
         <section className="contact_us">
             
             </section>             
