84ddaca827cfff9150257622e5e5109b1f44b9b2
MikeProduce <Gomez_miguelomar@yahoo.com>
2022-11-30 09:07:17 -0600

    really close to figuring it out

--- Chat-Application/src/pages/CreateUser.jsx
+++ Chat-Application/src/pages/CreateUser.jsx
@@ -1,11 +1,13 @@
 import { Navigate } from "react-router-dom";
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import axios from "axios";
+// import { get } from "http";
 
 
 export function CreateUser() {
     const[username, setUsername]= useState('');
     const[userpassword, setUserPassword]= useState('');
+    const [data, setData] = useState([]);
 
         const handleusername =(event)=>{
             const username=event.target.value;
@@ -18,18 +20,41 @@ export function CreateUser() {
             console.log(userpassword);
             setUserPassword(userpassword);
         }
-
+        const loadData = async () => {
+            const response = await axios.get("http://localhost:5000/api/get");
+            setData(response.data);
+            
+        };
+    
+    
         const submitUser= async (e)=> {
             e.preventDefault(); 
             const userdata ={ username:username, password:userpassword };
+            loadData();
+            // getInput();
             
-            
-            await axios.post('http://127.0.0.1:5000/createUser', userdata )
-            .then(result=>{ 
-                console.log(result);
+            data.map((loginInfo) => {
+                const usernames = loginInfo.username;
+                console.log(usernames);
+                
+            })
 
-            });
+            if (usernames === username){
+                console.log('theres a match')
+            } else {
+               axios.post('http://127.0.0.1:5000/createUser', userdata )
+                    .then(result=>{ 
+                        console.log(result);});
+                
+            }
+            
+            
+            
         }
+        // const getInput = () => {
+           
+          
+        // }
 
     return (
         <React.Fragment>
