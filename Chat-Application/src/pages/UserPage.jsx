9997fc1b9d251ea4aca51634fd39fac6b9df7df3
Sergio Gonzalez <89554725+sergiogonzalezyes@users.noreply.github.com>
2022-12-14 22:59:25 -0600

    start to modify UserPage to look like chat

--- Chat-Application/src/pages/UserPage.jsx
+++ Chat-Application/src/pages/UserPage.jsx
@@ -1,11 +1,44 @@
 export const UserPage = () => {
-  return( <div className="UserPageDiv">
-    <form action="/action_page.php" class="form-container">
-      <textarea placeholder="Type message.." name="msg" required></textarea>
-    </form>
-    <button type="submit" class="btn">Send</button>
-    <button type="button" class="btn cancel">cancel</button>
-          </div>
-          
-          )
+  return( 
+
+    <div>
+      <head>
+    <title>Chatbox</title>
+  </head>
+  <body>
+    <div id="chatbox">
+      <div id="contacts">
+        <h1>Contacts</h1>
+        <ul>
+          <li>John Doe</li>
+          <li>Jane Smith</li>
+          <li>Bob Johnson</li>
+        </ul>
+      </div>
+      <div id="messagesandinputform">
+      <div id="messages">
+        <h1>Messages</h1>
+        <ul>
+          <li>
+            <p><b>John Doe:</b> Hi, how are you?</p>
+          </li>
+          <li>
+            <p><b>Jane Smith:</b> I'm doing well, thanks. How about you?</p>
+          </li>
+          <li>
+            <p><b>John Doe:</b> I'm doing pretty good, thanks for asking.</p>
+          </li>
+        </ul>
+      </div>
+      <div id="input-form">
+        <form>
+          <textarea placeholder="Enter a message"></textarea>
+          <button type="submit">Send</button>
+        </form>
+      </div>
+      </div>
+    </div>
+  </body>
+    </div>
+  )
 };
