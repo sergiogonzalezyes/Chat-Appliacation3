564ffecbb2425318edf051a525e250091762dbf1
MikeProduce <Gomez_miguelomar@yahoo.com>
2022-12-18 09:02:47 -0600

    fixed!

--- Chat-Application/backend/server/index.js
+++ Chat-Application/backend/server/index.js
@@ -30,7 +30,7 @@ app.use(bodyParser.urlencoded({ extended: true })); // this allows incoming url 
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5000",
+        origin: "http://localhost:5173",
         methods: ["GET", "POST"],
     },
 });
@@ -38,7 +38,7 @@ io.on("connection", (socket) => {
     console.log(`User Connected: ${socket.id}`);
 });
 
-server.listen(5173, () => {
+server.listen(5001, () => {
     console.log("server is running");
 });
 
--- Chat-Application/src/pages/UserPage.jsx
+++ Chat-Application/src/pages/UserPage.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useRef } from "react";
 import { useEffect } from "react";
 import io from 'socket.io-client'
-const socket = io.connect('http://localhost:3001')
+const socket = io.connect('http://localhost:5001')
 
 
 
