cc01eb3b271d878fe7a97a1f549b68f25a7b4f97
MikeProduce <Gomez_miguelomar@yahoo.com>
2022-12-27 13:42:40 -0600

    I DID IT!!!!!

--- Chat-Application/backend/server/index.js
+++ Chat-Application/backend/server/index.js
@@ -191,17 +191,17 @@ app.get("/UserPage", verifyJWT, (req, res) => {
     });
 });
 
-// io.on("connection", (socket) => {
-//     console.log(socket.id);
-//     socket.on("send_username", (username) => {
-//         socket.broadcast.emit("receive_username", username);
-//     });
+io.on("connection", (socket) => {
+    console.log(socket.id);
+    // socket.on("send_username", (username) => {
+    //     socket.broadcast.emit("receive_username", username);
+    // });
 
-//     socket.on("send_message", (data) => {
-//         console.log(data);
-//         socket.broadcast.emit("receive_message", data);
-//     });
-// });
+    socket.on("send_message", (data) => {
+        console.log(data);
+        socket.broadcast.emit("receive_message", data);
+    });
+});
 
 // Airplay occupies the port 5000 for sending and receiving requests!!!
 // App awaits to be started in port 5000. Remember if you are on mac OS, turn off receiving for AirPlay
